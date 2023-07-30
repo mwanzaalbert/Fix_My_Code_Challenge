f31ff4ddd94b3924b54dd222833d468625b88354
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2023-07-30 17:51:58 +0300

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 /*
     Print a square with the character #
     
@@ -6,21 +7,14 @@
     of the program.
 */
 
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
+if (process.argv.length <= 2 || isNaN(process.argv[2])) {
+    process.stderr.write("Missing or invalid argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1);
 }
 
 const size = parseInt(process.argv[2], 10);
-
-if (isNaN(size) || size <= 0) {
-    process.stderr.write("Invalid argument. Please provide a valid positive integer.\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
-}
 
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
